604923694ca31cfd50a7015cbfa5789c2030de63
Lucas Andrei <lucas23marin@gmail.com>
2023-11-01 20:44:37 -0300

    adicionando mais funções membros

--- vector2.h
+++ vector2.h
@@ -5,13 +5,14 @@ class Vector2
     public:
 
     double x;
-    double y = x = 0;
+    double y;
 
-    Vector2(double x, double y);
+    Vector2(double x1, double y1);
     Vector2();
-    Vector2 operator+ (Vector2 &v);
-    Vector2 operator* (Vector2 &v);
-    Vector2 operator- (Vector2 &v);
+    Vector2 & operator= (Vector2 &v);
+    Vector2 & operator+ (Vector2 &v);
+    double & operator* (Vector2 &v);
+    Vector2 & operator- (Vector2 &v);
     Vector2 produtoEscalar(const Vector2 &v);
 
 
